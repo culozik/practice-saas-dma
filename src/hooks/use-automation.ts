5f2ff4b3b2c1fcaa10c25ed2e41eee0db5da30c5
culozik <eugenbah@gmail.com>
2025-04-04 18:34:05 +0300

    feat(hooks): add useCreateAutomation and useMutationData hooks
    - Add useMutationData hook for handling mutations with react-query.
    - Add useCreateAutomation hook for creating new automations.

---
+++ src/hooks/use-automation.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { createAutomations } from "@/actions/automations";
+import { useMutationData } from "./use-mutation-data";
+
+export const useCreateAutomation = () => {
+	const { isPending, mutate } = useMutationData(
+		["create-automation"],
+		() => createAutomations(),
+		"user-automations"
+	);
+
+	return {
+		isPending,
+		mutate,
+	};
+};
