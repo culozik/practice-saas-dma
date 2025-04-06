4ed8c3856675198e627df45636cef319997f16eb
culozik <eugenbah@gmail.com>
2025-04-06 21:54:00 +0300

    feat(ui): add useCreateAutomation hook to CreateAutomation component
    - Integrate useCreateAutomation hook into CreateAutomation component.
    - Enable automation creation functionality with unique mutation IDs.

--- src/components/global/create-automation/index.tsx
+++ src/components/global/create-automation/index.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+"use client";
+
+import { useMemo } from "react";
+import { v4 } from "uuid";
 
 import Loader from "@/components/global/loader";
 import { Button } from "@/components/ui/button";
 import { AutomationDuoToneWhite } from "@/icons";
+import { useCreateAutomation } from "@/hooks/use-automation";
 
 const CreateAutomation = () => {
-	// TODO: Create the automation in the database using mutate
+	const mutationId = useMemo(() => v4(), []);
+
+	const { isPending, mutate } = useCreateAutomation(mutationId);
+
 	return (
-		<Button className="lg:px-10 py-6 bg-gradient-to-br hover:opacity-80 text-white rounded-full from-[#3352CC] font-medium to-[#1C2D70]">
-			<Loader state={false}>
+		<Button
+			className="lg:px-10 py-6 bg-gradient-to-br hover:opacity-80 text-white rounded-full from-[#3352CC] font-medium to-[#1C2D70]"
+			onClick={() =>
+				mutate({
+					name: "New Automation",
+					id: mutationId,
+					createdAt: new Date(),
+					keywords: [],
+				})
+			}
+		>
+			<Loader state={isPending}>
 				<AutomationDuoToneWhite />
 				<p className="lg:inline hidden">Create an Automation</p>
 			</Loader>
